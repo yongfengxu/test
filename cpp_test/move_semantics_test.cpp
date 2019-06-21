739f3bdd15c114b84aee88c1ba8bb808c3146296
Charlie Xu <charlie.xu@ericsson.com>
2019-06-21 15:58:16 +0800

    re-write move semantics function
    Change-Id: I3a090fdf2ab24551a66fa0012bed0785f067aadf

--- cpp_test/move_semantics_test.cpp
+++ cpp_test/move_semantics_test.cpp
@@ -6,38 +6,63 @@ class A
 {
 public:
     A() = default;
+    ~A()
+    {
+        cout << "destructor" << endl;
+    }
 
     explicit A(int i):data(i)
     {
-        cout << "normal constuctor1!" << endl;
+        cout << "normal constuctor!" << endl;
         pi = &data;
     }
 
-    A(const A &a)
+    A(const A& a)
     {
-        data = a.data;
         cout << "copy constructor!" << endl;
+        data = a.data;
         pi = &data;
     }
 
-    A(A &&a)
+    A(A&& a)
     {
         cout << "move constructor!" << endl;
         pi = a.pi;
+        data = a.data;
 
         a.pi = NULL;
         a.data = 0;
     }
 
-    A operator+(const A &a)
+    A& operator+(const A& a)
     {
-        A temp(data + a.data);
-        cout << endl << "operator+ called! show temp!" << endl;
+        cout << "operator+ called!" << endl;
+        this->data += a.data;
 
-        temp.show();
-        cout << endl;
-        return temp;
+        return *this;
     }
+
+    A& operator=(const A& a)
+    {
+        cout << "operator= called!" << endl;
+        this->data = a.data;
+        this->pi = a.pi;
+
+        return *this;
+    }
+
+    A& operator=(A&& a)
+    {
+        cout << "move operator= called!" << endl;
+        this->data = a.data;
+        this->pi = a.pi;
+
+        a.data = 0;
+        a.pi = nullptr;
+
+        return *this;
+    }
+
 
     void show() const
     {
@@ -53,15 +78,34 @@ private:
 int main()
 {
     int i = 99;
+
+    //normal constructor
     A a(10);
     a.show();
 
     A b(i);
     b.show();
 
+    //copy constructor
     A c(b);
     c.show();
 
+    //copy constructor
     A d(b+c);
     d.show();
+
+    //move constructor
+    A e = std::move(d);
+    e.show();
+    d.show();
+
+
+    A f(std::move(e));
+    f.show();
+
+    A g(2);
+    g.show();
+    g = std::move(f);
+    g.show();
+
 }
