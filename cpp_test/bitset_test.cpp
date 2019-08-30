863fe4ec44cce79800779d6f07538ae44ddf3f9a
Charlie Xu <charlie.xu@ericsson.com>
2019-08-30 17:15:34 +0800

    [cpp test] add bieset test
    Change-Id: If8afcef1e1c7e84c2984e40082d2079b2e36df1c

--- cpp_test/bitset_test.cpp
+++ cpp_test/bitset_test.cpp
@@ -3,10 +3,28 @@
 
 
 using namespace std;
+enum class ApplyActionValue : uint32_t
+{
+    DROP = 1,
+    FORD = 2,
+    BUFF = 4,
+    NOCP = 8,
+    BUFFNOCP = 12,
+    DUPL = 16
+};
+
+using ApplyActions = std::bitset<8>;
+
+void fun(const ApplyActions actions)
+{
+    if (actions.test(0))
+    {
+        cout << "bit 0 is 1" << endl;
+    }
+}
 
 int main()
 {
-    using ApplyActions = std::bitset<8>;
 
 
     ApplyActions actions("0101");
@@ -19,6 +37,7 @@ int main()
     std::cout << "size = " << actions.size() << std::endl;
 
 
+    fun(ApplyActions(static_cast<uint32_t>(ApplyActionValue::BUFFNOCP)));
 
 
 
