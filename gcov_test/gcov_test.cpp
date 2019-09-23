fdd3460506081536d2809f11f9e1f4fcce03b3f8
Charlie Xu <charlie.xu@ericsson.com>
2019-09-23 14:14:26 +0800

    [GCOV] add gcov test
    Change-Id: If572859fd96d7b7cd604b101df17155b7351f4f0

---
+++ gcov_test/gcov_test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+void fun()
+{
+    std::cout << "hello world" << std::endl;
+}
+
+void fun1()
+{
+    std::cout << "fun1" << std::endl;
+}
+
+int main()
+{
+    fun();
+}
---
+++ gcov_test/readme.txt
@@ -0,0 +1,1 @@
+g++ gcov_test.cpp -o gcov_test -fprofile-arcs -ftest-coverage
