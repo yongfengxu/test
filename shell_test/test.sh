20f8bf7997bd15a7e6e3f5664f0eb0c370ee7185
Charlie Xu <charlie.xu@ericsson.com>
2020-06-12 09:50:55 +0800

    [Shell] test the result of specific line
    Change-Id: Ia8daafdd26af2b64b1ac5b0608bd7f46a0075c35

--- shell_test/test.sh
+++ shell_test/test.sh
@@ -5,6 +5,8 @@ echo $SCRIPT_DIR
 
 WORK_DIR=$SCRIPT_DIR
 
+CLEANUP=false
+
 # Workspace where to find the git repository, build and run tests
 WORKSPACE="$( cd "$( dirname "$WORK_DIR/../../.." )" && pwd )"
 echo $WORKSPACE
@@ -13,4 +15,13 @@ TEST_LOG_DIR=$WORKSPACE/logs
 STORE_LOGS="/proj/epg/sgw_dt/massive_logs"
 
 
+function hello {
+    echo "hello"
+}
+
+[ "$CLEANUP" = true ]&& hello
+
+#if [ "$CLEANUP" = true ]; then
+   #hello
+#fi
 
